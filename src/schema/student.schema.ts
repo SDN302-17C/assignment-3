e7c7dca2430ff15248429f62c417400f81e9919b
Quốc Chương <quocchuong2609@gmail.com>
2024-10-17 18:21:13 +0700

    update: setup `student` schema

---
+++ src/schema/student.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({ timestamps: true })
+export class Student {
+	@Prop({ required: true })
+	studentCode: string;
+
+	@Prop({ required: true })
+	fullName: string;
+
+	@Prop({ required: true, unique: true })
+	email: string;
+
+	@Prop({ required: true })
+	address: string;
+
+	@Prop({ required: true })
+	gender: 'male' | 'female';
+
+	@Prop({ default: false })
+	isDeleted: boolean;
+}
+
+export const StudentSchema = SchemaFactory.createForClass(Student);
