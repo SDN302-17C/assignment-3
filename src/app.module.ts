4f0abae40aba8870ab6a9eb4d2c67bd97c5cebd6
Quốc Chương <quocchuong2609@gmail.com>
2024-10-17 19:26:27 +0700

    update: connect mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './students/student.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASE_URL ?? 'mongodb://localhost:27017/students'),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
