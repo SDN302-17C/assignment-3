6937927160a7d7ea54864afc90525ff96b8a75c6
Quốc Chương <quocchuong2609@gmail.com>
2024-10-21 08:04:55 +0700

    update: get info

--- src/info/info.controller.ts
+++ src/info/info.controller.ts
@@ -1,14 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
-import { InfoService } from './info.service';
 import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Info')
 @Controller('info')
 export class InfoController {
-  constructor(private readonly infoService: InfoService) {}
-
   @Get()
   async getAll() {
-    return await this.infoService.getAll();
+    return {
+      data: {
+        fullName: 'Đinh Quốc Chương',
+        studentCode: 'QE170097',
+      },
+    };
   }
 }
--- src/info/info.module.ts
+++ src/info/info.module.ts
@@ -1,15 +1,7 @@
 import { Module } from '@nestjs/common';
 import { InfoController } from './info.controller';
-import { InfoService } from './info.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { InfoSchema } from 'src/schema/info.schema';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'Info', schema: InfoSchema }]),
-  ],
   controllers: [InfoController],
-  providers: [InfoService],
-  exports: [InfoService],
 })
 export class InfoModule {}
